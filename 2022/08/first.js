2c0d4c5968cd2e36ccc31df618b34c560b6837bc
Wyatt Carss <carss.w@gmail.com>
2022-12-08 01:38:14 -0500

    actual solution for day 8 part 1

--- 2022/08/first.js
+++ 2022/08/first.js
@@ -16,69 +16,46 @@ let load = data => {
   return trees;
 }
 
-let calculate_tree_visibility = (trees, x, y) => {
-  // to the west
-  if (x === 0 || (trees[x-1][y].wv && trees[x-1][y].h < trees[x][y].h)) {
-    trees[x][y].wv = true;
-    trees[x][y].hw = trees[x][y].h;
-    trees[x][y].v = true;
+let check_horizontal_line = (trees, y) => {
+  let max = -Infinity;
+  for (let i = 0; i < trees.length; i++) {
+    if (trees[i][y].h > max) {
+      trees[i][y].v = true;
+      max = trees[i][y].h;
+    }
   }
-  // to the east
-  if (x === trees.length - 1 || (trees[x+1][y].ev && trees[x+1][y].h < trees[x][y].h)) {
-    trees[x][y].ev = true;
-    trees[x][y].hw = trees[x][y].h;
-    trees[x][y].v = true;
-  }
-  // to the north
-  if (y === 0 || (trees[x][y-1].nv && trees[x][y-1].h < trees[x][y].h)) {
-    trees[x][y].nv = true;
-    trees[x][y].hn = trees[x][y].h;
-    trees[x][y].v = true;
-  }
-  // to the south (we check both in the center)
-  if (y === trees[0].length - 1 || (trees[x][y+1].sv && trees[x][y+1].h < trees[x][y].h)) {
-    trees[x][y].sv = true;
-    trees[x][y].hs = trees[x][y].h;
-    trees[x][y].v = true; 
-  }
-  return trees[x][y].v;
-}
-
-let check_horizontal_line = (trees, x, x2, y) => {
-  for (let i = x; i < x2; i++) {
-    calculate_tree_visibility(trees, i, y);
+  max = -Infinity;
+  for (let i = trees.length-1; i >= 0; i--) {
+    if (trees[i][y].h > max) {
+      trees[i][y].v = true;
+      max = trees[i][y].h;
+    }
   }
 }
 
-let check_vertical_line = (trees, x, y, y2) => {
-  for (let i = y; i < y2; i++) {
-    calculate_tree_visibility(trees, x, i);
+let check_vertical_line = (trees, x) => {
+  let max = -Infinity;
+  for (let i = 0; i < trees.length; i++) {
+    if (trees[x][i].h > max) {
+      trees[x][i].v = true;
+      max = trees[x][i].h;
+    }
   }
-}
-
-let check_ring = (trees, x, y) => {
-  // top
-  check_horizontal_line(trees, x, (trees.length)-x, y);
-  // bottom
-  check_horizontal_line(trees, x, (trees.length)-x, (trees[0].length-1)-y);
-  // left
-  check_vertical_line(trees, x, y, (trees[0].length)-y);
-  // right
-  check_vertical_line(trees, (trees.length-1)-x, y, (trees[0].length)-y);
-}
-
-let round_up = num => {
-  return parseInt(Math.ceil(num));
+  max = -Infinity;
+  for (let i = trees.length-1; i >= 0; i--) {
+    if (trees[x][i].h > max) {
+      trees[x][i].v = true;
+      max = trees[x][i].h;
+    }
+  }
 }
 
 let check_forest = (trees) => {
-  let i = 0;
-  let j = 0;
-  while (i <= trees.length/2 && j <= trees[0].length/2) {
-    console.log('checking ring:', i, ', ', j);
-    check_ring(trees, i, j);
-    if (i <= trees.length/2) i++;
-    if (j <= trees[0].length/2) j++;
+  for (let i = 0; i < trees[0].length; i++) {
+    check_horizontal_line(trees, i);
+  }
+  for (let i = 0; i < trees.length; i++) {
+    check_vertical_line(trees, i);
   }
 };
 
@@ -113,7 +90,7 @@ let print_forest = (trees, filter_by_vis) => {
 }
 
 
-let trees = load(readData('beta'));
+let trees = load(readData('data'));
 console.log(trees);
 print_forest(trees, false);
 
